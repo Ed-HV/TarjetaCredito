9c72684639a7432b911cf824e7d748524f4faeb6
Eduardo_H. <157440749+Ed-HV@users.noreply.github.com>
2024-11-26 17:19:38 -0600

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Principal {
-}
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el límite de la tarjeta: ");
+        double limite = lectura.nextDouble();
+        TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
+
+        int salir = 1;
+        double totalCompras = 0; // Variable para acumular el total de las compras
+
+        while (salir != 0) {
+            System.out.println("Escriba la descripción de la compra:");
+            String descripcion = lectura.next();
+
+            System.out.println("Escriba el valor de la compra:");
+            double valor = Double.valueOf(lectura.next());
+
+            Compra compra = new Compra(valor, descripcion);
+            boolean compraRealizada = tarjeta.LanzarCompra(compra);
+
+            if (compraRealizada) {
+                totalCompras += valor; // Suma el valor de la compra al total
+                System.out.println("Compra realizada!");
+                System.out.println("Escriba 0 para salir o 1 para continuar");
+                salir = lectura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                salir = 0;
+            }
+        }
