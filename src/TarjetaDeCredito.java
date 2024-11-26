722311e9884b75ed1a60dc87a06981e590d8aa7a
Eduardo_H. <157440749+Ed-HV@users.noreply.github.com>
2024-11-26 17:20:13 -0600

    Update TarjetaDeCredito.java

--- src/TarjetaDeCredito.java
+++ src/TarjetaDeCredito.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class TarjetaDeCredito {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TarjetaDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> listaDeCompras;
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompras = new ArrayList<>();
+    }
+    public boolean LanzarCompra(Compra compra){
+        if (this.saldo >= compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.listaDeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListaDeCompras() {
+        return listaDeCompras;
+    }
 }
